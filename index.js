c71ce2ef75c5526a53dd80f1008f7cea0f6162bc
mubeen207 <mubeenulislam207@gmail.com>
2026-04-11 22:25:10 +0500

    new changing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import router from "./routes/index.js";
 const app = express();
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "*" })); 
 app.use(express.json());
 app.use("/api", router);
 
