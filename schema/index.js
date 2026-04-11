29f0daa19d9ec20d6ea53a0717df64043ec670bc
mubeen207 <mubeenulislam207@gmail.com>
2026-04-11 23:06:28 +0500

    userSchema Added

--- schema/index.js
+++ schema/index.js
@@ -8,4 +8,13 @@ const todoSchema = new mongoose.Schema(
   { timestamps: true },
 );
 
-export default todoSchema;
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true },
+);
+
+export default { todoSchema, userSchema };
