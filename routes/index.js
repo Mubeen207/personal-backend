b56bf368f1f6edb40047509e4028d176066e0395
mubeen207 <mubeenulislam207@gmail.com>
2026-04-11 23:11:49 +0500

    user fatching done

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,14 @@ import getTodos from "./todo/get.js";
 import postTodo from "./todo/post.js";
 import deleteTodo from "./todo/delete.js";
 import updateTodo from "./todo/update.js";
+import getUsers from "./user/get.js";
 const router = express.Router();
 
 router.get("/todos", getTodos);
 router.post("/todo", postTodo);
 router.put("/todo/:id", updateTodo);
 router.delete("/todo/:id", deleteTodo);
+
+router.get("/users", getUsers);
 
 export default router;
---
+++ routes/user/get.js
@@ -0,0 +1,17 @@
+import mongoose from "../../db/index.js";
+import userSchema from "../../schema/index.js";
+
+const getUsers = async (req, res) => {
+  const Users = mongoose.model("Users", userSchema);
+  try {
+    const allUsers = await Users.find();
+    res.status(200).send({
+      message: "Users fatched successfully",
+      data: allUsers,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export default getUsers;
